cd36badcf89330c9abba41914a371e5274046461
Daisuke Nakahara <daisuken@msn.com>
2020-12-03 06:43:25 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@ subscriptionId=$(az account show --query id -o tsv)
 spName='OpenPlatform-TSI-SP'-"$subscriptionId"
 
 echo 'Subscription ID ' $servicePrincipalObjectId
-echo 'SPName ' spName
+echo 'SPName ' $spName
