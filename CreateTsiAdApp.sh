d554f33f9d3b8e25a45141a66e372444e2c5eb04
Daisuke Nakahara <daisuken@msn.com>
2020-12-04 07:11:57 -0800

    Update CreateTsiAdApp.sh

--- CreateTsiAdApp.sh
+++ CreateTsiAdApp.sh
@@ -4,17 +4,9 @@ subscriptionId=$(az account show --query id -o tsv)
 adAppName='OpenPlatform-TSI-SP'-"$subscriptionId"
 echo $("adAppName : $adAppName")
 
-az ad app list --show-mine --query "[?displayName=='$adAppName'].appId"
+# az login --identity
 
-az login --identity
-
-servicePrincipalAppId=$(az ad app list --show-mine --query "[?displayName=='$adAppName'].appId" -o tsv)
-echo $("servicePrincipalAppId : ${servicePrincipalAppId}")
-
-if [ -z "$servicePrincipalAppId" ]; then
-    servicePrincipalAppId=$(az ad app create --display-name $adAppName --identifier-uris "https://$adAppName"  --oauth2-allow-implicit-flow true --required-resource-accesses '[{"resourceAppId":"120d688d-1518-4cf7-bd38-182f158850b6","resourceAccess":[{"id":"a3a77dfe-67a4-4373-b02a-dfe8485e2248","type":"Scope"}]}]' --query appId -o tsv)
-fi
-
+servicePrincipalAppId=$(az ad app create --display-name $adAppName --identifier-uris "https://$adAppName"  --oauth2-allow-implicit-flow true --required-resource-accesses '[{"resourceAppId":"120d688d-1518-4cf7-bd38-182f158850b6","resourceAccess":[{"id":"a3a77dfe-67a4-4373-b02a-dfe8485e2248","type":"Scope"}]}]' --query appId -o tsv)
 servicePrincipalObjectId=$(az ad sp list --show-mine --query "[?appDisplayName=='$adAppName'].objectId" -o tsv)
 echo $("servicePrincipalObjectId : $servicePrincipalObjectId")
 
