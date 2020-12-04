9d879cd126e27ddde5966e885906fbf8b0a6eca6
Daisuke Nakahara <daisuken@msn.com>
2020-12-04 06:10:21 -0800

    Update CreateTsiAdApp.sh

--- CreateTsiAdApp.sh
+++ CreateTsiAdApp.sh
@@ -18,4 +18,5 @@ fi
 servicePrincipalSecret=$(az ad app credential reset --append --id $servicePrincipalAppId --credential-description "TSISecret" --query password -o tsv)
 servicePrincipalTenantId=$(az ad sp show --id $servicePrincipalAppId --query appOwnerTenantId -o tsv)
 
-json="{\"appId\":\"$servicePrincipalAppId\",\"spSecret\":\"$servicePrincipalSecret\",\"tenantId\":\"$servicePrincipalTenantId\",\"spObjectId\":\"$servicePrincipalObjectId\"}" > $AZ_SCRIPTS_OUTPUT_PATH
+json="{\"appId\":\"$servicePrincipalAppId\",\"spSecret\":\"$servicePrincipalSecret\",\"tenantId\":\"$servicePrincipalTenantId\",\"spObjectId\":\"$servicePrincipalObjectId\"}"
+echo "$json" > $AZ_SCRIPTS_OUTPUT_PATH
