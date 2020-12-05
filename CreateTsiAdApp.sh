e3bbd2e7dbeb5c51b99c7389a9626c1f5853a668
Daisuke Nakahara <daisuken@msn.com>
2020-12-04 17:13:46 -0800

    Update CreateTsiAdApp.sh

--- CreateTsiAdApp.sh
+++ CreateTsiAdApp.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+az extension add --name timeseriesinsights --yes
+
+webSiteName=$1
+resGroup=$2
+
 subscriptionId=$(az account show --query id -o tsv)
-adAppName='OpenPlatform-TSI-SP'-"$subscriptionId"
+adAppName='IoTPnPWS-TSI-AD-App'-"$subscriptionId"
 # az login --identity
-servicePrincipalAppId=$(az ad app create --display-name $adAppName --identifier-uris "https://$adAppName"  --oauth2-allow-implicit-flow true --required-resource-accesses '[{"resourceAppId":"120d688d-1518-4cf7-bd38-182f158850b6","resourceAccess":[{"id":"a3a77dfe-67a4-4373-b02a-dfe8485e2248","type":"Scope"}]}]' --query appId -o tsv)
+servicePrincipalAppId=$(az ad app create --display-name $adAppName --identifier-uris "https://${adAppName}"  --oauth2-allow-implicit-flow true --required-resource-accesses '[{"resourceAppId":"120d688d-1518-4cf7-bd38-182f158850b6","resourceAccess":[{"id":"a3a77dfe-67a4-4373-b02a-dfe8485e2248","type":"Scope"}]}]' --query appId -o tsv)
 servicePrincipalObjectId=$(az ad sp list --show-mine --query "[?appDisplayName=='$adAppName'].objectId" -o tsv)
 if [ -z "$servicePrincipalObjectId" ]; then
     servicePrincipalObjectId=$(az ad sp create --id $servicePrincipalAppId --query objectId -o tsv)
@@ -13,5 +18,9 @@ servicePrincipalTenantId=$(az ad sp show --id $servicePrincipalAppId --query app
 echo $("servicePrincipalSecret :   $servicePrincipalSecret")
 echo $("servicePrincipalTenantId : $servicePrincipalTenantId")
 json="{\"appId\":\"$servicePrincipalAppId\",\"spSecret\":\"$servicePrincipalSecret\",\"tenantId\":\"$servicePrincipalTenantId\",\"spObjectId\":\"$servicePrincipalObjectId\"}"
-echo $("$json")
-echo "$json" > $AZ_SCRIPTS_OUTPUT_PATH
+
+az ad app update --id $servicePrincipalAppId --reply-urls "https://${webSiteName}.azurewebsites.net/"
+az webapp config appsettings set --name $webSiteName --resource-group $resGroup --settings Azure__TimeSeriesInsights__clientId=$servicePrincipalAppId
+az webapp config appsettings set --name $webSiteName --resource-group $resGroup --settings Azure__TimeSeriesInsights__tsiSecret=$servicePrincipalSecret
+az webapp config appsettings set --name $webSiteName --resource-group $resGroup --settings Azure__TimeSeriesInsights__clientId=$servicePrincipalAppId
+az timeseriesinsights access-policy create -g $resGroup --environment-name {env} -n "TSI-SP" --principal-object-id $servicePrincipalObjectId --roles Reader
