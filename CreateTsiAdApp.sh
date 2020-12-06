0f892ab3c54c8a3408e78cf7bd4d9a16ef1b7f04
Daisuke Nakahara <daisuken@msn.com>
2020-12-06 01:06:59 -0800

    Update CreateTsiAdApp.sh

--- CreateTsiAdApp.sh
+++ CreateTsiAdApp.sh
@@ -14,9 +14,10 @@ subscriptionId=$(az account show --query id -o tsv)
 adAppName='IoTPnPWS-TSI-AD-App'-"$subscriptionId"
 # az login --identity
 servicePrincipalAppId=$(az ad app create --display-name $adAppName --identifier-uris "https://${adAppName}"  --oauth2-allow-implicit-flow true --required-resource-accesses '[{"resourceAppId":"120d688d-1518-4cf7-bd38-182f158850b6","resourceAccess":[{"id":"a3a77dfe-67a4-4373-b02a-dfe8485e2248","type":"Scope"}]}]' --query appId -o tsv)
-servicePrincipalObjectId=$(az ad sp list --show-mine --query "[?appDisplayName=='$adAppName'].principalObjectId" -o tsv)
+#servicePrincipalObjectId=$(az ad sp list --show-mine --query "[?appDisplayName=='$adAppName'].principalObjectId" -o tsv)
+servicePrincipalObjectId=$(az ad sp show --id "https://${adAppName}" --query objectId -o tsv)
 if [ -z "$servicePrincipalObjectId" ]; then
-    servicePrincipalObjectId=$(az ad sp create --id $servicePrincipalAppId --query principalObjectId -o tsv)
+    servicePrincipalObjectId=$(az ad sp create --id $servicePrincipalAppId --query objectId -o tsv)
 fi
 servicePrincipalSecret=$(az ad app credential reset --append --id $servicePrincipalAppId --credential-description "TSISecret" --query password -o tsv)
 servicePrincipalTenantId=$(az ad sp show --id $servicePrincipalAppId --query appOwnerTenantId -o tsv)
